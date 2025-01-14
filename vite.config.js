33eb985b8ea92d498d377804dac56fee95f3cdca
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-01-14 18:00:32 -0400

    Primer commit v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://Wilgrey2004.github.io/Tic-Tac-Toe-React.git",
+});
